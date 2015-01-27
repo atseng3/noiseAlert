fdcd7e72ef8631ae62790aacfc13f033f8ac6534
Albert Tseng <atseng3@gmail.com>
2015-01-26 21:54:14 -0800

    add basic functionality

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global NoiseAlert, $*/
+
+
+window.NoiseAlert = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    NoiseAlert.init();
+});
